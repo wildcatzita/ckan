40bbbe5a9a33e310b33d240f8d026c51a8c95dd3
Artem Bazykin <artem.bazykin@linkdigital.com.au>
2017-03-03 16:07:25 +0200

    Added migration for group table

---
+++ ckan/migration/versions/086_group_id_name_unique.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+import warnings
+
+from sqlalchemy import exc as sa_exc
+from sqlalchemy import *
+from migrate import *
+from migrate.changeset.constraint import UniqueConstraint
+
+
+def upgrade(migrate_engine):
+    # ignore reflection warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=sa_exc.SAWarning)
+        metadata = MetaData()
+        metadata.bind = migrate_engine
+        group_table = Table('group', metadata, autoload=True)
+        unique_id_name_constraint = UniqueConstraint(
+            'id', 'name', table=group_table)
+        unique_id_name_constraint.create()
